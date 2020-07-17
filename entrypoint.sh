11efae8b4ac5be9ddccee39667dcc06860a1f5e5
Antoine Hébert <hantoine02@gmail.com>
2020-07-17 14:04:59 -0400

    Fix logs in output zip filew

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# Redirect everything to stderr, except fd 3 to stdout
+exec 3>&2 4>&3 3>&1 1>&4-
+
 # Take Overleaf zip file from stdin
 cat > paper.zip
 
@@ -12,5 +16,5 @@ unzip paper.zip -d output-dir
 mv compilation-dir/*.bbl output-dir/
 
 # Output zip to stdout
-cd output-dir && zip -r - *
+cd output-dir && zip -r - * >&3
 
